89cae48f8983f84663058677c0c406a2e8efd4a5
Ilia Trubitsin <lionen89@yandex.ru>
2023-12-16 12:25:59 +0300

    fix:last bugs

--- src/hooks/useCounter.ts
+++ src/hooks/useCounter.ts
@@ -3,25 +3,24 @@ import { useEffect, useState, useRef } from 'react'
 export const useCounter = (value: number, defaultValue: number, eventCount: number): number => {
   const [counter, setCounter] = useState<number>(defaultValue)
   const duration: number = 20
-  const delay = 3
+  const delay: number = 5
 
   const versionRef = useRef<number>(0)
 
   useEffect(() => {
     if (!value || value === counter) return
 
-    const step = Math.round(Math.abs((value - defaultValue) / eventCount / delay))
+    const step = Math.ceil(Math.abs((value - defaultValue) / eventCount / delay))
     versionRef.current += 1
     const currentVersion = versionRef.current
 
     const intervalId = setInterval(() => {
       if (currentVersion === versionRef.current) {
         setCounter((prev) => {
-          const shouldIncrease = value > counter && prev + step <= value
-          const shouldDecrease = value < counter && prev - step >= value
-
-          if (shouldIncrease || shouldDecrease) {
-            return shouldIncrease ? prev + step : prev - step
+          if (value > counter && prev + step <= value) {
+            return prev + step
+          } else if (value < counter && prev - step >= value) {
+            return prev - step
           } else {
             return value
           }
